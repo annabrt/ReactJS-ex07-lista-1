328b223953333137b6f245cc33ce22bb4eda4272
Anna Bertochi <anafbrtc@gmail.com>
2024-10-20 00:15:49 -0300

    Add files via upload
    Exercício 07 da lista 1.

---
+++ ex07l1/src/App.jsx
@@ -0,0 +1,10 @@
+import Desconto  from './components/Desconto'
+import './App.css'
+
+function App(){
+  return(
+    <Desconto temDesconto={true}/>
+  )
+}
+
+export default App
---
+++ ex07l1/src/components/Desconto.jsx
@@ -0,0 +1,5 @@
+function Desconto(props){
+    return <h1>{props.temDesconto ? "Desconto Aplicado" : "Sem Desconto"}</h1>
+}
+
+export default Desconto
